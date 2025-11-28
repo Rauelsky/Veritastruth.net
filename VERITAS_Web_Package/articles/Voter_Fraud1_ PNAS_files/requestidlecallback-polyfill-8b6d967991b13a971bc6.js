9024dcfdf2987b001cee70c9446d4f607f6a7cdc
Rauelsky <rauel@veritastruth.net>
2025-11-28 12:14:09 -0600

    Delete VERITAS_Web_Package/articles/Voter_Fraud1_ PNAS_files directory

--- VERITAS_Web_Package/articles/Voter_Fraud1_ PNAS_files/requestidlecallback-polyfill-8b6d967991b13a971bc6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{837:function(n,e){window.requestIdleCallback=window.requestIdleCallback||function(n){var e=Date.now();return setTimeout((function(){n({didTimeout:!1,timeRemaining:function timeRemaining(){return Math.max(0,50-(Date.now()-e))}})}),1)},window.cancelIdleCallback=window.cancelIdleCallback||function(n){clearTimeout(n)}}}]);
-//# sourceMappingURL=requestidlecallback-polyfill-8b6d967991b13a971bc6.js.map
